08cabacc2b35a70f7cea4fbd1fae5eb6a1f11c98
chandra821 <73953659+chandra821@users.noreply.github.com>
2022-12-25 23:12:04 +0530

    poc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React,{Component} from 'react'
 import './App.css';
+import { withTranslation } from 'react-i18next';
+import i18next from 'i18next';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  handleClick(lang) {
+    i18next.changeLanguage(lang)
+  }
+  
+  render() {
+    const { t } = this.props;
+
+    return (
+      <div className="App">
+        <nav style={{ width: '100%', padding: '.5rem', backgroundColor: 'gray'}}>
+          <button onClick={() => this.handleClick('en')} >
+            English
+        </button>
+          <button onClick={() => this.handleClick('ko')} >
+            Korean
+        </button>
+          <button onClick={() => this.handleClick('chi')} >
+            Chinese
+       </button>
+       <button onClick={() => this.handleClick('hin')} >
+            Hindi
+       </button>
+       <button onClick={() => this.handleClick('spn')} >
+            Spanish
+       </button>
+        </nav>
+        <header className="App-header">
+          <div>
+            {/* <h1><iframe width="560" height="315" src="https://www.youtube.com/embed/cHqxgLhOl5Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></h1> */}
+          <h3 style={{ fontSize: "1rem", color: "red"}}>{t('chandu.1')}</h3>
+            <h3 style={{ fontSize: "1rem", color: "yellow"}}>{t('Thanks.1')}</h3>
+            <h3 style={{ fontSize: "1rem", color: "green"}}>{t('Why.1')}</h3>
+          </div>
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default withTranslation()(App);
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React,{Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './i18next'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Suspense fallback={(<div>Loading ~~~~~~~~~~</div>)}>
     <App />
-  </React.StrictMode>
+  </Suspense>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
